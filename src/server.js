18416b3f071a784dc207bf4e66001d1e18a57aa5
jostar222 <whtjdgus911@gmail.com>
2024-08-17 20:27:38 +0900

    chat with websockets -WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import Websocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,8 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new Websocket.Server({ server });
+
+server.listen(3000, handleListen);
