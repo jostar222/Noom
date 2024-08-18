506683744266903fdadde40de32285bca8688c66
jostar222 <whtjdgus911@gmail.com>
2024-08-18 22:08:43 +0900

    socketio -Installing SocketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import Websocket from "ws";
+//import Websocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,13 +11,16 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
+wsServer.on("connection", (socket) => {
+  console.log(socket);
+});
+
+/*
 const wss = new Websocket.Server({ server });
-
 const sockets = [];
-
 wss.on("connection", (socket) => {
   sockets.push(socket);
   socket["nickname"] = "Anon"; //익명
@@ -36,6 +40,8 @@ wss.on("connection", (socket) => {
       break;
     }
   });
-});
+}); 
+*/
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
